f3838245b675e0f2d318c073c9cd8b874b2500db
QianqianDu1122 <qdu6@illinois.edu>
2025-01-06 23:18:05 -0600

    add code for creating the trial designs- consistent trial design and randomized trial design

--- Codes/Make-tables-figures/make_td.R
+++ Codes/Make-tables-figures/make_td.R
@@ -13,8 +13,7 @@ td_with_blocks <- add_blocks(td_single_input)
 
 td_with_blocks$trial_design
 
-viz(td_with_blocks, type = "block_id")
-
+viz(td_with_blocks, type = "rate")
 
 
 
@@ -23,12 +22,118 @@ viz(td_with_blocks, type = "block_id")
 data(exp_data)
 exp_data
 
+exp_data$
+
 data(rate_info)
 rate_info
 
-td <- assign_rates(exp_data, rate_info)
+td <- assign_rates(exp_data, rate_info) 
 
 viz(td)
+
+
+###############################################################
+###====== Create the trial design based on our demand (Latin Square Trial Design) ======###
+###############################################################
+
+n_plot_info <-
+  prep_plot(
+    input_name = "N",
+    unit_system = "imperial",
+    machine_width = 30,
+    section_num = 1,
+    harvester_width = 20,
+    headland_length = 30,
+    side_length = 60
+  )
+
+
+exp_data <-
+  make_exp_plots(
+    input_plot_info = n_plot_info,
+    boundary_data = system.file("extdata", "boundary-simple1.shp", package = "ofpetrial"),
+    abline_data = system.file("extdata", "ab-line-simple1.shp", package = "ofpetrial"),
+    abline_type = "free")
+
+
+plot_info <-
+  prep_plot(
+    input_name = "N",
+    unit_system = "imperial",
+    machine_width = 60,
+    section_num = 1,
+    harvester_width = 30,
+    plot_width = 60
+  )
+
+rate_info <- prep_rate(
+  plot_info,
+  gc_rate = 180,
+  unit = "lb",
+  rates = c(0, 60, 120, 180, 240, 270)
+)
+
+
+###--- create the trial design based on the exp_data and rate_info that we just wrote ---###
+
+td <- assign_rates(exp_data, rate_info) 
+
+viz(td)
+
+
+
+###############################################################
+###====== Create the trial design based on our demand (Randomized Trial Design) ======###
+###############################################################
+
+n_plot_info_R <-
+  prep_plot(
+    input_name = "N",
+    unit_system = "imperial",
+    machine_width = 30,
+    section_num = 1,
+    harvester_width = 20,
+    headland_length = 30,
+    side_length = 60
+  )
+
+
+exp_data_R <-
+  make_exp_plots(
+    input_plot_info = n_plot_info_R,
+    boundary_data = system.file("extdata", "boundary-simple1.shp", package = "ofpetrial"),
+    abline_data = system.file("extdata", "ab-line-simple1.shp", package = "ofpetrial"),
+    abline_type = "free")
+
+
+plot_info_R <-
+  prep_plot(
+    input_name = "N",
+    unit_system = "imperial",
+    machine_width = 60,
+    section_num = 1,
+    harvester_width = 30,
+    plot_width = 60
+  )
+
+rate_info_R <- prep_rate(
+  plot_info,
+  gc_rate = 180,
+  unit = "lb",
+  rates = c(0, 60, 120, 180, 240, 270),
+  design_type = "rb"
+)
+
+
+###--- create the trial design based on the exp_data and rate_info that we just wrote ---###
+
+td_R <- assign_rates(exp_data_R, rate_info_R) 
+
+viz(td_R)
+
+
+
+
 
 #--- change rates of some strips ---#
 strip_ids <- 1:5
